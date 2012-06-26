a2e1a6b1526640b9c92bdd237e4768ad98d9c136
Vibhaj Rajan <vibhaj8@gmail.com>
2012-06-26 13:33:15 +0530

    GeoStore 2.0 : improving Kestrel UI

--- dev/build.bat
+++ dev/build.bat
@@ -1,4 +1,4 @@
 @echo off
 type styles\*.css > ..\ui\css\geostore-ui.css
-type ..\ui\js\jquery-1.6.1.min.js ..\ui\js\jquery.tmpl.min.js ..\ui\js\jquery.cookie.js ..\ui\js\json2.js ..\ui\js\jquery.expander.js ..\..\services\snowblozm\js\Snowblozm.class.js ..\..\services\firespark\jquery\dev\jquery-firespark.js init.js helpers\*.js workflows\*.js > ..\ui\js\geostore-ui.js
+type ..\ui\js\jquery-1.6.1.min.js ..\ui\js\jquery.tmpl.min.js ..\ui\js\jquery.cookie.js ..\ui\js\json2.js ..\ui\js\jquery.expander.js ..\..\services\snowblozm\js\Snowblozm.class.js ..\..\services\firespark\jquery\dev\jquery-firespark.js init.js geostore\*.js > ..\ui\js\geostore-ui.js
 pause
