adf7618a3f85886679d203606d2ee3487447bd24
Vibhaj Rajan <vibhaj8@gmail.com>
2012-06-26 19:56:13 +0530

    GeoStore 2.2 : added .html format in kestrel.core.response

--- kestrel/core/response.py
+++ kestrel/core/response.py
@@ -8,11 +8,22 @@ def run(kwargs):
 	if kwargs.has_key('page') and kwargs.get('page') == None:
 		del kwargs['page']
 	
-	if kwargs.get('format', 'html') == 'json':
+	# respond json data
+	if kwargs.get('format', None) == 'json':
 		return HttpResponse(
 			simplejson.dumps(kwargs.get('data', {})), 
 			mimetype='application/json'
 		)
+	# respond kestrel base html
+	elif kwargs.get('format', None) == 'html':
+		data = kwargs.get('data', {})
+		data['kestrel'] = 'html'
+		return render_to_response(
+			kwargs.get('page', 'page/home') + '.html', 
+			data, 
+			context_instance = RequestContext(kwargs.get('request', None))
+		)
+	# respond base html
 	else:
 		return render_to_response(
 			kwargs.get('page', 'page/home') + '.html', 
